25247aaf160a4f59a3a6e27d5cb491056b0b08d2
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2022-10-13 16:47:23 +0530

    Time: 330 ms (20.78%), Space: 93.3 MB (88.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,9 @@ public:
         int profit=0,i=0;
         for(int j=1; j<prices.size(); j++){
             if(prices[i]>prices[j])
-                i=j;
-            if(prices[j]-prices[i]>profit)
-                profit=prices[j]-prices[i];
+                i=j; // Updating least price 
+            if(prices[j]-prices[i]>profit) // Comparing profit of that particular day
+                profit=prices[j]-prices[i]; // Updating profit
         }
         return profit;
     }
