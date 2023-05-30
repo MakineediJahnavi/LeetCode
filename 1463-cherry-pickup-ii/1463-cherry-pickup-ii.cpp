8c5e6f21edead8a101dea6407fe14c8357818db7
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-05-30 18:06:26 +0530

    Time: 269 ms (14.48%), Space: 9.2 MB (97.24%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,35 +3,37 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int> (m,0)));
+        vector<vector<int>> dp(m,vector<int> (m,0));
+        vector<vector<int>> temp(m,vector<int> (m,0));
         for(int j1=0; j1<m; j1++) {
             for(int j2=0; j2<m; j2++) {
                 if(j1==j2)
-                    dp[n-1][j1][j2]=grid[n-1][j1];
+                    dp[j1][j2]=grid[n-1][j1];
                 else 
-                    dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+                    dp[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
             }
         }
         for(int i=n-2; i>=0; i--) {
             for(int j1=0; j1<m; j1++) {
                 for(int j2=0; j2<m; j2++) {
                     int maxi=-1e8;
-        for(int dj1=-1; dj1<=1; dj1++) {
-            for(int dj2=-1; dj2<=1; dj2++) {
-                int value=0;
-                if(j1==j2)
-                    value=grid[i][j1];
-                else
-                    value=grid[i][j1]+grid[i][j2];
-                if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m)
-                    value+=dp[i+1][j1+dj1][j2+dj2];
-                maxi=max(value,maxi);
-            }
-        }
-        dp[i][j1][j2]=maxi;
+                    for(int dj1=-1; dj1<=1; dj1++) {
+                        for(int dj2=-1; dj2<=1; dj2++) {
+                            int value=0;
+                            if(j1==j2)
+                                value=grid[i][j1];
+                            else
+                                value=grid[i][j1]+grid[i][j2];
+                            if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m)
+                                value+=dp[j1+dj1][j2+dj2];
+                            maxi=max(value,maxi);
+                        }
+                    }
+                    temp[j1][j2]=maxi;
                 }
             }
+            dp=temp;
         }
-        return dp[0][0][m-1];
+        return dp[0][m-1];
     }
 };
