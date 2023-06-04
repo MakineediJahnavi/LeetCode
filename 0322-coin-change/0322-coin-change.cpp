79507d70b0864dd72d03285b18d34025cf798294
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-06-04 15:07:53 +0530

    Time: 109 ms (61.28%), Space: 40.7 MB (38.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,40 +1,27 @@
 class Solution {
-private:
-    int count(int i,int target,vector<int> &coins,vector<vector<int>> &dp) {
-        if(i==0) {
-            if(target%coins[0]==0)
-                return target/coins[0];
-            return 1e9;
-        }
-        if(dp[i][target]!=-1)
-            return dp[i][target];
-        int notake=count(i-1,target,coins,dp);
-        int take=INT_MAX;
-        if(coins[i]<=target)
-            take=1+count(i,target-coins[i],coins,dp);
-        return dp[i][target]=min(take,notake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> dp(amount+1,0);
         for(int target=0; target<=amount; target++){
             if(target%coins[0]==0)
-                dp[0][target]=target/coins[0];
+                dp[target]=target/coins[0];
             else
-               dp[0][target]=1e9; 
+               dp[target]=1e9; 
         }
         for(int i=1; i<n; i++) {
+            vector<int> cur(amount+1,0);
             for(int j=0; j<=amount; j++) {
-                int notake=dp[i-1][j];
+                int notake=dp[j];
                 int take=INT_MAX;
                 if(coins[i]<=j)
-                    take=1+dp[i][j-coins[i]];
-                dp[i][j]=min(take,notake);
+                    take=1+cur[j-coins[i]];
+                cur[j]=min(take,notake);
             }
+            dp=cur;
         }
-        if(dp[n-1][amount]>=1e9)
+        if(dp[amount]>=1e9)
             return -1;
-        return dp[n-1][amount];
+        return dp[amount];
     }
 };
