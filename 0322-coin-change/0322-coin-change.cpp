824c021d216d60dfbcf8ac9ff011764d8cb4121d
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-06-04 14:55:53 +0530

    Time: 173 ms (18.74%), Space: 45.1 MB (33.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -17,10 +17,24 @@ private:
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=count(n-1,amount,coins,dp);
-        if(ans>=1e9)
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int target=0; target<=amount; target++){
+            if(target%coins[0]==0)
+                dp[0][target]=target/coins[0];
+            else
+               dp[0][target]=1e9; 
+        }
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<=amount; j++) {
+                int notake=dp[i-1][j];
+                int take=INT_MAX;
+                if(coins[i]<=j)
+                    take=1+dp[i][j-coins[i]];
+                dp[i][j]=min(take,notake);
+            }
+        }
+        if(dp[n-1][amount]>=1e9)
             return -1;
-        return ans;
+        return dp[n-1][amount];
     }
 };
