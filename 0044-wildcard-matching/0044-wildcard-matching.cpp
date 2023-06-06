bfe115979a635c610a6f093a509ea8c09dfcac8b
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-06-06 19:10:29 +0530

    Time: 91 ms (42.54%), Space: 8.8 MB (83.30%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,10 +3,8 @@ public:
     bool isMatch(string s, string p) {
         int n=s.length();
         int m=p.length();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true;
-        for(int i=1; i<=n; i++)
-            dp[i][0]=false;
+        vector<bool> dp(m+1,false);
+        dp[0]=true;
         for(int j=1; j<=m; j++){
             bool flag=true;
             for(int k=1; k<=j; k++){
@@ -15,18 +13,20 @@ public:
                     break;
                 }
             }
-            dp[0][j]=flag;
+            dp[j]=flag;
         }
         for(int i=1; i<=n; i++) {
+            vector<bool> cur(m+1,false);
             for(int j=1; j<=m; j++) {
                 if(s[i-1]==p[j-1] || p[j-1]=='?')
-                    dp[i][j]=dp[i-1][j-1];
+                    cur[j]=dp[j-1];
                 else if(p[j-1]=='*')
-                    dp[i][j]=dp[i][j-1]|dp[i-1][j];
+                    cur[j]=cur[j-1]|dp[j];
                 else
-                    dp[i][j]=false;
+                    cur[j]=false;
             }
+            dp=cur;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
