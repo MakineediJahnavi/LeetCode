24a5e4893c5515dc9de326e59b5b10b0038a264b
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-06-25 13:14:30 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -9,44 +9,6 @@ using namespace std;
 
 class Solution{
     int mod = 1003;
-private:
-    int count(int i,int j,int isTrue,string s,vector<vector<vector<int>>> &dp) {
-        if(i>j)
-            return 0;
-        if(i==j) {
-            if(isTrue)
-                return s[i]=='T';
-            return s[i]=='F';
-        }
-        if(dp[i][j][isTrue]!=-1)
-            return dp[i][j][isTrue];
-        long long ans=0;
-        for(int k=i+1; k<j; k+=2) {
-            long long tl=count(i,k-1,1,s,dp);
-            long long tr=count(k+1,j,1,s,dp);
-            long long fl=count(i,k-1,0,s,dp);
-            long long fr=count(k+1,j,0,s,dp);
-            if(s[k]=='&') {
-                if(isTrue)
-                    ans=(ans+(tl*tr)%mod)%mod;
-                else 
-                    ans=(ans+(tl*fr)%mod+(fl*tr)%mod+(fl*fr)%mod)%mod;
-            }
-            else if(s[k]=='|'){
-                if(isTrue)
-                    ans=(ans+(tl*tr)%mod+(tl*fr)%mod+(fl*tr)%mod)%mod;
-                else
-                    ans=(ans+(fl*fr)%mod)%mod;
-            }
-            else{
-                if(isTrue)
-                    ans=(ans+(tl*fr)%mod+(fl*tr)%mod)%mod;
-                else
-                    ans=(ans+(tl*tr)%mod+(fl*fr)%mod)%mod;
-            }
-        }
-        return dp[i][j][isTrue]=ans%mod;
-    }
 public:
     int countWays(int N, string s){
         int n=s.length();
