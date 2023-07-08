03fe3aec3155ff790314596dffcf39c6ba397e74
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-07-08 10:52:49 +0530

    Time: 51 ms (29.33%), Space: 117.1 MB (6.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,17 +1,35 @@
 class Solution {
+private: 
+    int maxlen(vector<int> prefix_sum,int q) {
+        int l=0,r=prefix_sum.size()-1;
+        int len=0;
+        while(l<=r) {
+            int mid=(l+r)/2;
+            if(q==prefix_sum[mid]){
+                len=mid+1;
+                break;
+            }
+            else if(prefix_sum[mid]<q){
+                len=mid+1;
+                l=mid+1;
+            }
+            else
+                r=mid-1;
+        }
+        return len;
+    }
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         vector<int> ans;
         sort(nums.begin(),nums.end());
-        for(auto q: queries) {
-            int len=0;
-            for(auto it: nums) {
-                if(q>=it) {
-                    q-=it;
-                    len++;
-                }
-            }
-            ans.push_back(len);
+        vector<int> prefix_sum;
+        int s=0;
+        for(auto it: nums) {
+            s+=it;
+            prefix_sum.push_back(s);
+        }
+        for(auto q: queries){
+            ans.push_back(maxlen(prefix_sum,q));
         }
         return ans;
     }
