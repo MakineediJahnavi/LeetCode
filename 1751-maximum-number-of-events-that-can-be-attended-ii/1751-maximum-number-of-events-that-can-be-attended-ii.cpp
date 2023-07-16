6c73cfef7eff1c488293ba53b43666933dd16eab
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-07-16 11:14:27 +0530

    Time: 272 ms (98.26%), Space: 80.4 MB (51.97%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    int nextindex(int i,vector<pair<int,pair<int,int>>> &m){
+        int end=m[i].second.first;
+        int l=0,r=m.size();
+        while(l<r){
+            int mid=(l+r)/2;
+            if(m[mid].first>end){
+                r=mid;
+            }
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+    int fun(int i,vector<pair<int,pair<int,int>>> &m,int k,vector<vector<int>> &dp) {
+        if(i==m.size())
+            return 0;
+        if(k==0)
+            return 0;
+        if(dp[i][k]!=-1)
+            return dp[i][k];
+        int next=nextindex(i,m);
+        int notake = fun(i+1,m,k,dp);
+        int take=m[i].second.second+fun(next,m,k-1,dp);
+        return dp[i][k]=max(take,notake);
+    }
+public:
+    int maxValue(vector<vector<int>>& events, int k) {
+        int n=events.size();
+        vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
+        vector<pair<int,pair<int,int>>> m;
+        for(auto it: events) {
+            m.push_back({it[0],{it[1],it[2]}});
+        }
+        sort(m.begin(),m.end());
+        return fun(0,m,k,dp);
+    }
+};
