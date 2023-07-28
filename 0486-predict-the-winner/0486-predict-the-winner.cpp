c75ba543e71d4cfe439ceb9b121b8b3086c41266
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-07-28 08:42:38 +0530

    Time: 3 ms (70.47%), Space: 8.8 MB (7.07%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    int prediction(int i,int j,int flag,vector<int> &nums,vector<vector<vector<int>>> &dp) {
-        if(i>j)
-            return 0;
-        if(dp[i][j][flag]!=-1)
-            return dp[i][j][flag];
-        int score1=INT_MIN;
-        if(flag)
-            score1=max(nums[i]+prediction(i+1,j,0,nums,dp),nums[j]+prediction(i,j-1,0,nums,dp));
-        else
-            score1=min(prediction(i+1,j,1,nums,dp),prediction(i,j-1,1,nums,dp));
-        return dp[i][j][flag]=score1;
-    }
 public:
     bool PredictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n+1,vector<int>(2,0)));
         int sum=0;
         for(auto n: nums) {
             sum+=n;
         }
-        int score1=prediction(0,nums.size()-1,1,nums,dp);
+        for(int i=n-1; i>=0; i--) {
+            for(int j=1; j<=n; j++) {
+                for(int flag=0; flag<=1; flag++) {
+                    if(i>j)
+                        continue;
+                    int score1=INT_MIN;
+                    if(flag)
+                        score1=max(nums[i]+dp[i+1][j][0],nums[j-1]+dp[i][j-1][0]);
+                    else
+                        score1=min(dp[i+1][j][1],dp[i][j-1][1]);
+                    dp[i][j][flag]=score1;
+                }
+            }
+        }
+        int score1=dp[0][n][1];
         int score2=sum-score1;
         return (score1>=score2);
     }
