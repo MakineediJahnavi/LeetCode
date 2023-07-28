77f395adf94af84ec81b5a17fe7453c1c3adb111
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-07-28 14:29:56 +0530

    Time: 205 ms (37.54%), Space: 17.2 MB (28.78%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
-private: 
-    int min_jumps(int i,vector<int> &nums,vector<int> &dp) {
-        if(i==nums.size()-1)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int ans=1e9;
-        for(int j=1; j<=nums[i]; j++) {
-            if((i+j)>=nums.size())
-                break;
-            ans=min(ans,1+min_jumps(i+j,nums,dp));
-        }
-        return dp[i]=ans;
-    }
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
         if(n<=1)
             return 0;
-        vector<int> dp(n,-1);
-        return min_jumps(0,nums,dp);
+        vector<int> dp(n,0);
+        for(int i=n-2; i>=0; i--) {
+            int ans=1e9;
+            for(int j=1; j<=nums[i]; j++) {
+                if((i+j)>=nums.size())
+                    break;
+                ans=min(ans,1+dp[i+j]);
+            }
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
