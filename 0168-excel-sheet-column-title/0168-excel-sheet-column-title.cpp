6db74880e9b5c8e32eebbca2a22e952d901b7dce
Makineedi Jahnavi <85089860+MakineediJahnavi@users.noreply.github.com>
2023-08-22 22:23:01 +0530

    Time: 2 ms (49.78%), Space: 6 MB (37.93%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans="";
+        while(columnNumber) {
+            columnNumber--;
+            ans+=(char)((columnNumber%26)+'A');
+            columnNumber/=26;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
